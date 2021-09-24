46fc96552ee54ac072d06e856d83d41d89eeb823
Douglas-Gabriel <dou-ga@hotmail.com>
2021-09-23 21:54:20 -0300

    iniciar bot

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const Discord = require('discord.js')
-const config = require('./config.json')
+const { Client, Intents } = require('discord.js')
+const { token } = require('./config.json')
 
-const client = new Discord.Client()
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
-client.login(config.token)
+const prefix = '='
+
+client.on('message', function (message) {
+  if (message.author.bot) return
+  if (!message.content.startsWith(prefix)) return
+
+  const commandBody = message.content.slice(prefix.length)
+  const args = commandBody.split(' ')
+  const command = args.shift().toLowerCase()
+
+  if (command === 'ping') {
+    const timeTaken = Date.now() - message.createdTimestamp
+    message.reply('Pong!')
+    message.reply(timeTaken)
+    console.log('teste')
+  }
+})
+
+client.login(token)
